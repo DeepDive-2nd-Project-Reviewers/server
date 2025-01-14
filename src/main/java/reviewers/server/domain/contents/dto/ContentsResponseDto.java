82bc995ce357adc535fa71ea510ccc62a166639e
leedidu <dlwltn510@gmail.com>
2025-01-14 14:23:12 +0900

    refactor: @Setter 삭제 (#1)

--- src/main/java/reviewers/server/domain/contents/dto/ContentsResponseDto.java
+++ src/main/java/reviewers/server/domain/contents/dto/ContentsResponseDto.java
@@ -2,10 +2,8 @@ package reviewers.server.domain.contents.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class ContentsResponseDto {
     private Long contentId;
