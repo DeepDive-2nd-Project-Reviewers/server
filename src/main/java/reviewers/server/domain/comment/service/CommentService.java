44b5fac53b4869b2f5151da0124475d3558e0b9b
youngkwanglim <richard08@naver.com>
2025-01-15 11:07:29 +0900

    feat: commentRequest의 userId로 user 찾기

--- src/main/java/reviewers/server/domain/comment/service/CommentService.java
+++ src/main/java/reviewers/server/domain/comment/service/CommentService.java
@@ -2,9 +2,29 @@ package reviewers.server.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reviewers.server.domain.comment.dto.CommentResponse;
+import reviewers.server.domain.comment.entity.Comment;
+import reviewers.server.domain.comment.dto.CommentRequest;
+import reviewers.server.domain.comment.repository.CommentRepository;
+import reviewers.server.domain.review.entity.Review;
+import reviewers.server.domain.review.repository.ReviewInterface;
+import reviewers.server.domain.user.entity.User;
+import reviewers.server.domain.user.repository.UserRepository;
+import reviewers.server.global.exception.BaseErrorException;
+import reviewers.server.global.exception.ErrorType;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ReviewInterface reviewRepository;
+    private final CommentConverter commentConverter;
+
+    private User findUser(CommentRequest request) {
+        return userRepository.findById(request.getUserId())
+                .orElseThrow(() -> new BaseErrorException(ErrorType._NOT_FOUND_CONTENT));
+    }
 
 }
