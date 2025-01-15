feb826dc32a452855187e0b8b215179cfd6e7eb4
youngkwanglim <richard08@naver.com>
2025-01-15 10:54:09 +0900

    feat: entity에서 commentRequest로 변환

--- src/main/java/reviewers/server/domain/comment/service/CommentConverter.java
+++ src/main/java/reviewers/server/domain/comment/service/CommentConverter.java
@@ -3,6 +3,7 @@ package reviewers.server.domain.comment.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reviewers.server.domain.comment.dto.CommentRequest;
+import reviewers.server.domain.comment.dto.CommentResponse;
 import reviewers.server.domain.comment.entity.Comment;
 import reviewers.server.domain.review.entity.Review;
 import reviewers.server.domain.user.entity.User;
@@ -19,5 +20,12 @@ public class CommentConverter {
                 .build();
     }
 
-
+    // entity -> response
+    public CommentResponse toResponse(Comment comment) {
+        return CommentResponse.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .nickname(comment.getUser().getUsername())
+                .build();
+    }
 }
