f6b6a6d1f2723a0484a6d22b9291520fdcec14b2
wjd4204 <brian876373@gmail.com>
2025-01-16 17:26:49 +0900

    fix: Repository명 수정

--- src/main/java/reviewers/server/domain/review/repository/ReviewInterface.java
+++
@@ -1,7 +0,0 @@
-package reviewers.server.domain.review.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import reviewers.server.domain.review.entity.Review;
-
-public interface ReviewInterface extends JpaRepository<Review, Long> {
-}
