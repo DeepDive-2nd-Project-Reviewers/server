50faca2648b467290d2cb761b93e4033a2ae4630
SeoDong <79890834+SD-gif@users.noreply.github.com>
2025-01-16 10:42:20 +0900

    fix : Repository 어노테이션 제거

--- src/main/java/reviewers/server/domain/user/repository/UserRepository.java
+++ src/main/java/reviewers/server/domain/user/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package reviewers.server.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import reviewers.server.domain.user.entity.User;
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
