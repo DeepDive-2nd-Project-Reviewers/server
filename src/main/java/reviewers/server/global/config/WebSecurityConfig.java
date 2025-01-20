fef8bb8e915f669a27ab91f0ce7173bc3acfe5b2
youngkwanglim <richard08@naver.com>
2025-01-20 17:33:23 +0900

    fix: SecurityConfig 수정

--- src/main/java/reviewers/server/global/config/WebSecurityConfig.java
+++ src/main/java/reviewers/server/global/config/WebSecurityConfig.java
@@ -49,6 +49,7 @@ public class WebSecurityConfig {
                                 ,"/error").permitAll()
                         .requestMatchers("/api/v1/comments/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
                         .requestMatchers("/api/v1/contents/{contentId}/reviews/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
+                        .requestMatchers("/api/v1/contents/{contentId}/hearts/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
                         .requestMatchers(HttpMethod.GET,"/api/v1/contents/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
                         .requestMatchers("/api/v1/contents/**").hasRole(Role.ADMIN.name())
                         .requestMatchers("/api/v1/review/**").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
