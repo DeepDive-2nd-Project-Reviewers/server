922eb94f6344216afa62ef7eca1a4dd35523d2b0
wjd4204 <brian876373@gmail.com>
2025-01-24 01:57:49 +0900

    fix: redis 적

--- src/main/java/reviewers/server/global/config/RedisConfig.java
+++ src/main/java/reviewers/server/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -19,8 +20,15 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setPort(port);
+        config.setHostName(host);
+        config.setPassword(password);
         return new LettuceConnectionFactory(host, port);
     }
 
