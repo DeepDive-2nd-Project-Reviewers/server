08112e5f1157f7da799dce5e9f183b07b39168b7
youngkwanglim <richard08@naver.com>
2025-01-23 17:20:34 +0900

    style: 불필요한 import문 제거

--- src/main/java/reviewers/server/domain/comment/dto/CommentUpdateRequestDto.java
+++ src/main/java/reviewers/server/domain/comment/dto/CommentUpdateRequestDto.java
@@ -2,10 +2,8 @@ package reviewers.server.domain.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @NoArgsConstructor
